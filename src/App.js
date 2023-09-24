5e1a82b096edb200595f15c4614acf834565dd8f
gilles20393 <gilles1996@outlook.com>
2023-09-24 23:05:54 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import {MenuCard} from "./components/MenuCard";
+import {PRODUCTS_DATA} from "./data/data";
+
+function App() {
+    return (
+        <MenuCard products={PRODUCTS_DATA} />
+    );
+}
+
+export default App;
---
+++ src/components/MenuProduct.js
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+
+function ProductSize(props) {
+    const { product } = props;
+    if (!product.size) return;
+    return <span className="productSize" >{product.size ? " - " + product.size + "cl" : ""} </span>
+}
+function ProductNote(props) {
+    const { product } = props;
+    if (!product.note) return;
+    return <span className="note" >{product.note ? product.note : ""} </span>
+}
+export function MenuProduct(props) {
+    const { product } = props;
+    if (!product?.name) return;
+    return <container>
+        <div className="menuProduct">
+            <div>{product.name}
+                <ProductSize product={product} />
+            </div>
+            <div>{product.price} €</div>
+        </div>
+        <ProductNote product={product} />
+    </container>
+}
+
+MenuProduct.propTypes = {
+    products: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        price: PropTypes.string.isRequired,
+        size: PropTypes.string,
+        note: PropTypes.string
+    }).isRequired
+}
